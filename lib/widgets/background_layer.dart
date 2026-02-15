71b5f72506a451283e4a4d3a093da5098bab2c81
Nora Ashraf <noraa4033@gmail.com>
2026-02-15 14:38:11 +0200

    add img

--- lib/widgets/background_layer.dart
+++ lib/widgets/background_layer.dart
@@ -5,35 +5,12 @@ class BackgroundLayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
       height: double.infinity,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Color(0xFF0F2027), Color(0xFF203A43), Color(0xFF2C5364)],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: Stack(
-        children: [
-          _buildCircle(top: 100, left: 50, color: Colors.blueAccent),
-          _buildCircle(bottom: 150, right: 30, color: Colors.purpleAccent),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildCircle({double? top, double? left, double? right, double? bottom, required Color color}) {
-    return Positioned(
-      top: top, left: left, right: right, bottom: bottom,
-      child: Container(
-        width: 180,
-        height: 180,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: color.withOpacity(0.2),
-        ),
+      child: Image.asset(
+        'assets/imgs/p1.png', 
+        fit: BoxFit.cover, 
       ),
     );
   }
